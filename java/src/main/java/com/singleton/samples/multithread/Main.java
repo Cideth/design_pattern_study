6a48eb965be44d443910d54b3e1d8efa376bfe9c
bm150 <jbm20dev@gmail.com>
2024-03-21 21:40:39 +0900

    feat : 싱글톤 구현 예제 작성

---
+++ java/src/main/java/com/singleton/samples/multithread/Main.java
@@ -0,0 +1,33 @@
+package com.singleton.samples.multithread;
+
+/**
+ * 멀티스레드 환경에서 싱글톤
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+        threadFoo.start();
+        threadBar.start();
+
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.GetInstance();
+            System.out.println(singleton.hashCode());
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.GetInstance();
+            System.out.println(singleton.hashCode());
+        }
+    }
+
+
+}
