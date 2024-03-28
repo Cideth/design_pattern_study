3c35656765756a4f307a10455cb2de3cb3f99b23
bm150 <jbm20dev@gmail.com>
2024-03-28 17:46:05 +0900

    feat : builder pattern 예제 작성

--- java/src/main/java/com/builder/samples/humanbuilder/Human.java
+++ java/src/main/java/com/builder/samples/humanbuilder/Human.java
@@ -8,11 +8,35 @@ public class Human {
     private HairType hairType;
 
     public Human(List<Leg> legs, List<Eye> eyes, HairType hairType) {
-        if(legs.size() > 2 || eyes.size() > 2){
-           throw new IllegalArgumentException("다리의 개수 또는 눈의 개수가 잘못 되었습니다.");
+        if (legs.size() > 2 || eyes.size() > 2) {
+            throw new IllegalArgumentException("다리의 개수 또는 눈의 개수가 잘못 되었습니다.");
         }
         this.legs = legs;
         this.eyes = eyes;
+        this.hairType = hairType;
+    }
+
+    public List<Leg> getLegs() {
+        return legs;
+    }
+
+    public void setLegs(List<Leg> legs) {
+        this.legs = legs;
+    }
+
+    public List<Eye> getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(List<Eye> eyes) {
+        this.eyes = eyes;
+    }
+
+    public HairType getHairType() {
+        return hairType;
+    }
+
+    public void setHairType(HairType hairType) {
         this.hairType = hairType;
     }
 }
--- java/src/main/java/com/builder/samples/humanbuilder/HumanEye.java
+++ java/src/main/java/com/builder/samples/humanbuilder/HumanEye.java
@@ -1,6 +1,8 @@
 package com.builder.samples.humanbuilder;
 
 public class HumanEye extends Eye{
+    private static final String DEFAULT_COLOR = "dark";
+
     private AlignType type;
 
     public void setType(AlignType type) {
@@ -13,10 +15,7 @@ public class HumanEye extends Eye{
 
     @Override
     public void setColor(String color) {
-        if(color == null){
-            color = "dark";
-        }
-        this.setColor(color);
+        super.setColor(color == null ? DEFAULT_COLOR : color);
     }
 
     @Override
@@ -24,6 +23,15 @@ public class HumanEye extends Eye{
         if(size > 10){
             throw new IllegalArgumentException("사이즈는 10을 초과할 수 없습니다.");
         }
-        this.setSize(size);
+        super.setSize(size);
+    }
+
+    @Override
+    public String toString() {
+        return "HumanEye{" +
+                "type=" + type +
+                ", color='" + super.getColor() + '\'' +
+                ", size=" + super.getSize() +
+                '}';
     }
 }
