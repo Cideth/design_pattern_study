00e76ffa458d533a9893e3ce6d616f468076148d
bm150 <jbm20dev@gmail.com>
2024-04-01 17:06:15 +0900

    feat : proxy pattern 사용 예시 추가

--- java/src/main/java/com/proxy/samples/proxy/Proxy.java
+++ java/src/main/java/com/proxy/samples/proxy/Proxy.java
@@ -7,14 +7,19 @@ package com.proxy.samples.proxy;
  * </p>
  */
 public class Proxy implements IService{
+    String cacheValue;
     IService service1;
 
     @Override
     public String runSomething() {
         // 대리자는 실제 서비스의 메서드 호출 전후에 별도의 로직을 수행할 수도 있다.
         System.out.println("호출에 대한 흐름 제어가 주목적, 반환 결과를 그대로 전달.");
+        // 또는 아래와 같이 캐시용으로 사용이 가능하다.
+        if(cacheValue != null)
+            return cacheValue;
         // 대리자는 실제 서비스와 같은 이름을 가진 메서드를 호출하고, 그 값을 클라이언트에게 돌려준다.
         service1 = new Service();
-        return service1.runSomething();
+        cacheValue =  service1.runSomething();
+        return cacheValue;
     }
 }
